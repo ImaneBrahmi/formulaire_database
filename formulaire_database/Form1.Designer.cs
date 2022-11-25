feb0ac0d4eb34c0430505069e70c8b9ae39887c6
Hp <Hp@DESKTOP-C75HHEU>
2022-11-26 00:00:05 +0100

    Add project files.

---
+++ formulaire_database/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace formulaire_database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnselect = new System.Windows.Forms.Button();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // btnselect
+            // 
+            this.btnselect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnselect.Location = new System.Drawing.Point(418, 112);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(75, 23);
+            this.btnselect.TabIndex = 0;
+            this.btnselect.Text = "select";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // btninsert
+            // 
+            this.btninsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninsert.Location = new System.Drawing.Point(146, 256);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(75, 23);
+            this.btninsert.TabIndex = 1;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(227, 256);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(308, 256);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 3;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 166);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 33);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 205);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 33);
+            this.textBox2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Location = new System.Drawing.Point(146, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 107);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.btnselect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnselect;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ formulaire_database/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDB;
+using static System.Net.Mime.MediaTypeNames;
+using System.Data.OleDb;
+
+namespace formulaire_database
+{
+    public partial class Form1 : Form
+    {
+        static string chaine = @"Data Source=localhost;Initial Catalog=BDGED_copy;Integrated Security=True";
+        ";Data Source=C:\Users\Hp\Documents\formule.accdb"
+        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
+        static OleDbConnection cnx = new OleDblConnection(chaine);
+        static OleDbCommand cmd = new OleDbCommand();
+        static OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+        public Form1()
+        {
+            InitializeComponent();
+            RemplirGrid();
+        }
+        // REMPLISSAGE DE GRIDVIEW
+        public void RemplirGrid()
+        {
+            d.dap = new OleDbDataAdapter("select *from livre", d.conn);
+            d.dap.Fill(d.ds, "livre");
+            dataGridView1.DataSource = d.ds.Tables["livre"];
+        }
+
+
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            cmd.CommandText = "select * from livre";
+            cmd.Connection = cnx;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cnx.Close();
+        }
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if (txtReference.Text == "" || txttitre.Text == "" || txtprix.Text == "")
+            {
+                MessageBox.Show("remplir les champs")
+                    return;
+            }
+            cnx.Open();
+            cmd.Connection = cnx;
+            cmd.CommandText = "insert into livre(Reference,titre,prix) values('" + txtReference.Text + "','" + txttitre.Text + "','" + txtprix.Text + "') ";
+            cmd.ExecuteNonQuery();
+            cnx.Close();
+            for (int i = 0; i < d.ds.Tables["livre"].Rows.Count; i++)
+            {
+                if (txt.Refernce.Text == d.ds.Tables["livre"].Rows[i][0].TotString())
+                {
+                    MessageBox.Show("le livre deja existe");
+                    return;
+                }
+            }
+            d.ds.Tables["livre"].Rows.Add(d.ligne);
+            MessageBox.Show("le livre ajouter avec succes");
+            dataGridView1.DataSource = d.ds.Tables["livre"];
+            (this.Owner as Form1).btnDelete.Enabled = false;
+            (this.Owner as Form1).btnUpdate.Enabled = false;
+            (this.Owner as Form1).btnselect.Enabled = false;
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtReference.Text == "" || txttitre.Text == "" || txtprix.Text == "")
+            {
+                MessageBox.Show("remplir les champs")
+                    return;
+            }
+
+            if (txtRefernce.Text == "")
+            {
+                MessageBox.Show("errour");
+                return;
+            }
+            Boolean tr = false;
+            for (int i = 0; i < d.ds.Tables["livre"].Rows.Count; i++)
+            {
+                if (txtId_dossier.Refernce.Text == d.ds.Tables["livre"].Rows[i][0].TotString())
+                {
+                    tr = true;
+                    d.ds.Tables["livre"].Rows[i][1] = txtRefernce.Text;
+                    d.ds.Tables["livre"].Rows[i][2] = txttitre.Text;
+                    d.ds.Tables["livre"].Rows[i][3] = txtprix.Text;
+                    MessageBox.Show("le livre modifier avec succes");
+                    dataGridView1.DataSource = d.ds.Tables["livre"];
+                    break;
+
+                }
+            }
+            if (tr == false)
+            {
+                MessageBox.Show("le livre n'existe pas");
+            }
+            (this.Owner as Form1).btnDelete.Enabled = false;
+            (this.Owner as Form1).btnInsert.Enabled = false;
+            (this.Owner as Form1).btnselect.Enabled = false;
+
+            cnx.Open();
+            cmd.Connection = cnx;
+            cmd.CommandText = "update livre set titre='" + txttitre.Text + "update livre set prix='" + txtprix.Text + "' where Reference='" + txtRefernce.Text + "' ";
+            cnx.Close();
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            {
+                if (txtRefernce.Text == "")
+                {
+                    MessageBox.Show("errour");
+                    return;
+                }
+                Boolean tr = false;
+                for (int i = 0; i < d.ds.Tables["livre"].Rows.Count; i++)
+                {
+                    if (txtId_dossier.Refernce.Text == d.ds.Tables["livre"].Rows[i][0].TotString())
+                    {
+                        tr = true;
+                        d.ds.Tables["livre"].Rows[i].Delete();
+                        MessageBox.Show("le livre supprimer avec succes");
+                        dataGridView1.DataSource = d.ds.Tables["livre"];
+                        break;
+
+                    }
+                }
+                if (tr == false)
+                {
+                    MessageBox.Show("le livre n'exicte" pas);
+                }
+                (this.Owner as Form1).btnInsert.Enabled = false;
+                (this.Owner as Form1).btnUpdate.Enabled = false;
+                (this.Owner as Form1).btnselect.Enabled = false;
+                cnx.Open();
+                cmd.Connection = cnx;
+                cmd.CommandText = "delete from livre where Refernce='" + txtRefernce.Text + "' ";
+                cmd.ExecuteNonQuery();
+                cnx.Close();
+            }
+        }
+       
+    }
+ }
+
